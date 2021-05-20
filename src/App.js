81e402b594240b64fd6af92d80dfec831bda8a94
Andriy-Lyt <a.lyt@outlook.com>
2021-05-20 01:25:53 -0400

    1 commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchBar from "./compnents/searchBar";
+
+function App() {
+  return (
+    <SearchBar/>
+  );
+}
+
+export default App;
